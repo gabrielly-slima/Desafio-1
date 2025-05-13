2efa4473aee4613eab6fd0b84988315321848419
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-05-13 19:43:24 -0300

    iniciando codigo

---
+++ desafio-2/calculadora-simples.py
@@ -0,0 +1,20 @@
+numeros = []
+operacao_soma = "+"
+operacao_sub = "-"
+operacao_mult = "x"
+operacao_divisao = "/"
+
+def soma(numeros):
+    return sum(numeros)
+
+def sub(numeros)
+    resultado = [0]
+    for num in numeros [1:]:
+        resultado -= num
+
+def mult(numeros)
+    
+    
+def divi(numeros)
+    
+numeros = int(input("Digite os números com separação por espaços"))
