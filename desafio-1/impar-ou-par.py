9fa9b2dd4e2c7f6942bf52f60764b02e0341d2a1
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-05-13 17:57:42 -0300

    change name function, add .strip().upper() on input

--- desafio-1/impar-ou-par.py
+++ desafio-1/impar-ou-par.py
@@ -1,25 +1,24 @@
-def verificacao (numero):
+def eh_par (numero):
     return numero % 2 ==0 
 
 print("SEJA BEM-VINDO AO JOGO DO ÍMPAR OU PAR")
 
 while True:
-    entrada = input("Digite um número qualquer ou se quiser sair digite: SAIR")
-    print(" ")
+    entrada = input("Digite um número qualquer ou se quiser sair digite: SAIR").strip().upper()
     
     if entrada != "SAIR":
         try:
             numero = int(entrada)
         except ValueError:
-            print("Digite corretamente o que foi solicitado")
+            print("\nDigite corretamente o que foi solicitado\n")
             continue
 
-        if verificacao(numero):
-            print("O número que você escolheu é par!")
-            print(" ")
+        if eh_par(numero):
+            print("\nO número que você escolheu é par!\n")
+            
         else:
-            print("O número que você escolheu é ímpar!")
-            print(" ")
+            print("\nO número que você escolheu é ímpar!\n")
+
     else:
         print("Obrigado por jogar!")
         break
