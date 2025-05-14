5af74c87f00bb919a147ca985a5bc6d2cfc41c9d
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-05-14 11:47:29 -0300

    escrevi programação

--- desafio-2/calculadora-simples.py
+++ desafio-2/calculadora-simples.py
@@ -1,20 +1,55 @@
-numeros = []
-operacao_soma = "+"
-operacao_sub = "-"
-operacao_mult = "x"
-operacao_divisao = "/"
+entrada = input("Digite os números separando-os por espaços\n Para sair digite: sair").strip().upper()
 
-def soma(numeros):
-    return sum(numeros)
+try:
+    numeros = [float(n) for n in entrada.split()]
+except ValueError:
+    print("Por favor, insira apenas números válidos")
 
-def sub(numeros)
-    resultado = [0]
-    for num in numeros [1:]:
-        resultado -= num
+operador = input("Digite qual tipo de operação você quer realizar. Para somar: +\n Para subtrair: -\n Para multiplicar:*\n Para dividir:/\n").strip()
 
-def mult(numeros)
-    
-    
-def divi(numeros)
-    
-numeros = int(input("Digite os números com separação por espaços"))
+def somar(numeros):
+   return sum(numeros)
+        
+def subtrair(numeros):
+    resultado = numeros[0]
+    for n in numeros [1:]:
+        resultado -= n
+    return resultado
+
+def multiplicar(numeros):
+    resultado = 1
+    for n in numeros:
+        resultado *= n
+    return resultado
+
+def dividir(numeros):
+    resultado = numeros[0]
+    for n in numeros [1:]:
+        if n == 0:
+            print("Erro! Não é possível dividir por zero")
+            return None
+        resultado /= n
+    return resultado
+
+def exibir_resultado(resultado):
+    if resultado is not None:
+        print(f"O resultado é: {resultado}")
+
+if operador == "+":
+    resultado = somar(numeros)
+    exibir_resultado(resultado)
+
+elif operador == "-":
+    resultado = subtrair(numeros)
+    exibir_resultado(resultado)
+
+elif operador == "*":
+    resultado = multiplicar(numeros)
+    exibir_resultado(resultado)
+
+elif operador == "/":
+    resultado = dividir(numeros)
+    exibir_resultado(resultado)
+
+else:
+    print("Digite um operador válido")
