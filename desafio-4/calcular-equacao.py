165475c7542ff907dfc8ba90953149fbb753c8e3
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-09 14:27:48 -0300

    Update calcular-equacao.py

--- desafio-4/calcular-equacao.py
+++ desafio-4/calcular-equacao.py
@@ -1,27 +1,55 @@
-def values():
-    a_value = input("Digite o valor de 'a'\n")
-    operator_one = input("Digite o sinal da operação\n")
-    b_value = input("Digite o valor de 'b'\n")
-    operator_two = input("Digite o sinal da operação\n")
-    c_value = input("Digite o valor de 'c'\n")
+def values(degree):
+    while True:
+        try: 
+            a_value = int(input("Digite o valor de 'a'\n"))
+            b_value = int(input("Digite o valor de 'b'\n"))
 
-    return {"a":a_value,
-            "op1":operator_one,
-            "b":b_value,
-            "op2":operator_two,
-            "c":c_value}
+            if degree ==2:
+                c_value = int(input("Digite o valor de 'c'\n"))
+                return {"a":a_value,
+                "b":b_value,
+                "c":c_value}
+            
+            else:
+                return {"a":a_value,
+                "b":b_value,}
+
+        except ValueError:
+            error_message()
+            continue
 
 def calculate_first_degree(data):
-    print(f"Sua expressão é {data["a"]} {data["op1"]} {data["b"]} {data["op2"]} {data["c"]}")
+    print(f"Sua expressão é {data['a']} {data['b']} ")
+    print(f"f(x) = {data['a']}x {data['b']}")
+    result_x = data["a"]/data["b"]
+    print(f"f(x)= {result_x}")
 
+def error_message():
+    print("Caractere inválido! Insira o valor correto pedido")
 
-entrada = input("CALCULO DE EQUAÇÕES\n Digite 1 se quiser calcular uma equação do PRIMEIRO GRAU\n Digite 2 se quiser calcular uma equação do SEGUNDO GRAU\n")
+def main():
+    while True:
+        entrada = input("CALCULO DE EQUAÇÕES\n Digite 1 se quiser calcular uma equação do PRIMEIRO GRAU\n Digite 2 se quiser calcular uma equação do SEGUNDO GRAU\n Ou digite 0 para ENCERRAR o programa")
 
-if entrada == "1":
-    data = values()
-    calculate_first_degree(data)
-elif entrada =="2":
-    values()
+        if entrada =="1":
+            data = values(1)
+            calculate_first_degree(data)
+            break
 
+        elif entrada =="2":
+            values(2)
+            break
+
+        elif entrada =="0":
+            print("Encerrando...")
+            break
+        
+        else:
+            error_message()
+            continue
+
+        
+
+main()
 
 
