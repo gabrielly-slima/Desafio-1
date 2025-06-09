9ec129214d44bc1c35750d7348d4c16cd0a0bbfc
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-09 14:39:22 -0300

    add delta function

--- desafio-4/calcular-equacao.py
+++ desafio-4/calcular-equacao.py
@@ -2,6 +2,9 @@ def values(degree):
     while True:
         try: 
             a_value = int(input("Digite o valor de 'a'\n"))
+            if a_value ==0:
+                print("Erro! O valor de 'a' não pode ser zero")
+                continue
             b_value = int(input("Digite o valor de 'b'\n"))
 
             if degree ==2:
@@ -24,12 +27,20 @@ def calculate_first_degree(data):
     result_x = data["a"]/data["b"]
     print(f"f(x)= {result_x}")
 
+def calculate_second_degree(data):
+    print(f"Sua expressão é {data['a']} {data['b']} {data['c']}")
+    print(f"f(x)=")
+
+def delta_calculate(data):
+    result_delta = {data["b"]**2} - 4 * {data["a"]} * {data["c"]}
+    print(result_delta)
+
 def error_message():
     print("Caractere inválido! Insira o valor correto pedido")
 
 def main():
     while True:
-        entrada = input("CALCULO DE EQUAÇÕES\n Digite 1 se quiser calcular uma equação do PRIMEIRO GRAU\n Digite 2 se quiser calcular uma equação do SEGUNDO GRAU\n Ou digite 0 para ENCERRAR o programa")
+        entrada = input("CALCULO DE EQUAÇÕES\n Digite 1 se quiser calcular uma equação do PRIMEIRO GRAU\n Digite 2 se quiser calcular uma equação do SEGUNDO GRAU\n Ou digite 0 para ENCERRAR o programa\n")
 
         if entrada =="1":
             data = values(1)
@@ -38,6 +49,7 @@ def main():
 
         elif entrada =="2":
             values(2)
+            delta_calculate()
             break
 
         elif entrada =="0":
