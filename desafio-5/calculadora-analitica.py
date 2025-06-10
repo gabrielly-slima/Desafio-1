47fa0ec9562c66c2d2cd40c3d292389a2428b984
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-10 13:41:04 -0300

    Update calculadora-analitica.py

--- desafio-5/calculadora-analitica.py
+++ desafio-5/calculadora-analitica.py
@@ -1,27 +1,51 @@
 import matplotlib.pyplot as plt
-entrada = input("CALCULADORA DE GEOMETRIA ANALÍTICA\nDigite:\n1 para calcular a EQUAÇÃO GERAL DA RETA\n2 para calcular a EQUAÇÃO FUNDAMENTAL DA RETA\n3 para calcular a EQUAÇÃO REDUZIDA DA RETA")
-    if entrada == "1":
-        try:
-            main()
-        except ValueError:
-            print("bosta")
-        
-def main(): 
+
+def pedir_valores_x(): 
     while True:
-        valores_x = input("Digite as coordenadas do eixo das abscissas 'x' separando-as por espaços\n").split()
+        valores_x = input("Digite duas coordenadas do eixo das abscissas 'x' separando-as por espaços\n").split()
         if len(valores_x) > 1:
             try:
                 conver_coordenadas_x = [float(valores) for valores in valores_x]
-                print(conver_coordenadas_x)
-                break
+                return conver_coordenadas_x
+
+            except ValueError:
+                print("As coordenadas de x devem ser números válidos")
+        elif len(valores_x) > 2:
+            print("Você digitou coordenadas a mais, digite apenas duas")
+            continue
+        else:
+            print("Digite mais uma coordenada")
+        
+
+def pedir_valores_y(): 
+    while True:
+        valores_y = input("Digite as coordenadas do eixo das ordenadas 'y' separando-as por espaços\n").split()
+        if len(valores_y) > 1:
+            try:
+                conver_coordenadas_y = [float(valores) for valores in valores_y]
+                return conver_coordenadas_y
 
             except ValueError:
                 print("As coordenadas de x devem ser números válidos")
         else:
             print("Digite pelo menos mais de uma coordenada")
 
-         
 
+    
+
+
+def main():
+    entrada = input("CALCULADORA DE GEOMETRIA ANALÍTICA\nDigite:\n1 para calcular a EQUAÇÃO GERAL DA RETA\n2 para calcular a EQUAÇÃO FUNDAMENTAL DA RETA\n3 para calcular a EQUAÇÃO REDUZIDA DA RETA")
+    if entrada == "1":
+        try:
+            coordenadas_x = pedir_valores_x()
+            coordenadas_y = pedir_valores_y()
+            mostrar_valores(coordenadas_x,coordenadas_y)
+        except ValueError:
+            print("deu bosta")
+  
+         
+main()
 
     
 
