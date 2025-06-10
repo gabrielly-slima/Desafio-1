aad75bff3a82d0625093ed8404ce3fe4eab25969
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-10 14:49:11 -0300

    Update calculadora-analitica.py

--- desafio-5/calculadora-analitica.py
+++ desafio-5/calculadora-analitica.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-
 def pedir_valores_x(): 
     while True:
         valores_x = input("Digite duas coordenadas do eixo das abscissas 'x' separando-as por espaços\n").split()
@@ -37,6 +35,9 @@ def pedir_valores_y():
 def mostrar_valores(coordenadas_x,coordenadas_y):
     print(f"Sua reta será composta das seguintes coordenadas: X(a,b) = {coordenadas_x[0]},{coordenadas_x[1]} Y(a,b) = {coordenadas_y[0]},{coordenadas_y[1]}")
 
+def equacao_geral_da_reta(coordenadas_x,coordenadas_y):
+   
+
 def main():
     entrada = input("CALCULADORA DE GEOMETRIA ANALÍTICA\nDigite:\n1 para calcular a EQUAÇÃO GERAL DA RETA\n2 para calcular a EQUAÇÃO FUNDAMENTAL DA RETA\n3 para calcular a EQUAÇÃO REDUZIDA DA RETA\n")
     if entrada == "1":
@@ -44,6 +45,7 @@ def main():
             coordenadas_x = pedir_valores_x()
             coordenadas_y = pedir_valores_y()
             mostrar_valores(coordenadas_x,coordenadas_y)
+            equacao_geral_da_reta(coordenadas_x,coordenadas_y)
         except ValueError:
             print("deu bosta")
   
